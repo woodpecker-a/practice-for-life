5548a9391ed87d66273d3b049fce7d26c4f70638
Arafat Amin <arafat15-5321@diu.edu.bd>
2022-12-20 22:25:48 +0600

    PrimeCheck

---
+++ NumberTheory/PrimeCheck/Program.cs
@@ -0,0 +1,20 @@
+﻿bool IsPrime(double? n)
+{
+    if(n < 2) return false;
+    else if(n <= 3) return true;
+    else if(n % 2 == 0) return false;
+    else for (int i = 3; i * i <= n; i += 2) if (n % i == 0) return false;
+    return true;
+}
+
+static double? GetInput() => double.Parse(Console.ReadLine());
+
+var n = GetInput();
+
+if (n.HasValue)
+{
+    Console.ForegroundColor= ConsoleColor.Red;
+    Console.Write($"{n.Value} is");
+    Console.Write(IsPrime(n) ? " Prime" : " Not Prime");
+    Console.ForegroundColor= ConsoleColor.White;
+}
